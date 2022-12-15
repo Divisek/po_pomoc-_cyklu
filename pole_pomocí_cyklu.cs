2104049ce99fa66d5f4280e6e668c2d36d4d541b
Divisek <113785426+Divisek@users.noreply.github.com>
2022-12-15 10:52:41 +0100

    Add files via upload

---
+++ pole_pomocí_cyklu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole= new int[10];
+            pole[0] = 1;
+            for (int posun_indexu = 0; posun_indexu < 10; posun_indexu++)
+            {
+                pole[posun_indexu] = posun_indexu + 1;
+            }
+            for (int index = 0; index < pole.Length; index++)
+                Console.Write("{0}", pole[index]);
+        }
+    }
+}
